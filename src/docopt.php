9b9163273c4bb01178b6cd166e96499bff357bf4
StenidoS <os8333324@gmail.com>
2022-08-23 22:09:41 +0300

    modified

--- src/docopt.php
+++ src/docopt.php
@@ -21,6 +21,6 @@ Options:
 DOC;
 
 require('path/to/src/docopt.php');
-$args = Docopt::handle($doc, array('version'=>'Naval Fate 2.0'));
-foreach ($args as $k=>$v)
-    echo $k.': '.json_encode($v).PHP_EOL;    
+$args = Docopt::handle($doc, array('version' => 'Naval Fate 2.0'));
+foreach ($args as $k => $v)
+    echo $k . ': ' . json_encode($v) . PHP_EOL;
